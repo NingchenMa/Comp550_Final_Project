456f4a824ff8969efe651a48a6d39d4b83a09fb1
fbarn <fbarn@github.com>
2021-12-19 01:36:28 -0500

    finalized heval

--- heval.py
+++ heval.py
@@ -1,57 +1,86 @@
 from os import listdir
+from random import shuffle
+from csv import writer
 
-ms = sorted(listdir('./dec/MatchSum/'))
-pn = sorted(listdir('./dec/PNBERT/'))
-inputs = sorted(listdir('./in/'))
+ms = sorted(listdir('./dec/MatchSum/'), key=lambda f : int(f.split(".")[0]))
+pn = sorted(listdir('./dec/PNBERT/'), key=lambda f : int(f.split(".")[0]))
+pg = sorted(listdir('./dec/PGen/'), key=lambda f : int(f.split(".")[0]))
+ls = sorted(listdir('./dec/LoopSum/'), key=lambda f : int(f.split(".")[0]))
+rf = sorted(listdir('./ref/'), key=lambda f : int(f.split(".")[0]))
+inputs = sorted(listdir('./in/'), key=lambda f : int(f.split(".")[0]))
 
-msscore = [0,0]
-pnscore = [0,0]
+msscore = [[], []]
+pnscore = [[], []]
+pgscore = [[], []]
+lsscore = [[], []]
 
-for i in range(len(ms)):
-	msdata = []
-	pndata = []
+scores = [msscore, pnscore, pgscore, lsscore]
+
+for i in range(2):
+
+	shuf = [j for j in range(4)]
+	shuffle(shuf)
+
+	data = [[], [], [], []]
+
 	indata = []
+	rfdata = []
+
+
 	with open('./dec/MatchSum/' + ms[i], "r") as _file:
-		msdata = _file.readlines()
-	with open('./dec/PNBERT/' + ms[i], "r") as _file:
-		pndata = _file.readlines()
+		data[0] = _file.readlines()
+	with open('./dec/PNBERT/' + pn[i], "r") as _file:
+		data[1] = _file.readlines()
+	with open('./dec/PGen/' + pg[i], "r") as _file:
+		data[2] = _file.readlines()
+	with open('./dec/LoopSum/' + ls[i], "r") as _file:
+		data[3] = _file.readlines()
+	with open('./ref/' + rf[i], "r") as _file:
+		rfdata = _file.readlines()
 	with open('./in/' + inputs[i], "r") as _file:
 		indata = _file.readlines()
 
-	y = input("Press any key to view original article.")
+	print('\n')
 
-	for line in indata:
+	print("Original article:")
+
+	# for line in indata:
+	# 	print(line, end="")
+
+	print('\n')
+
+	print("Reference summary:")
+
+	for line in rfdata:
 		print(line, end="")
 
-	print()
+	print('\n')
 
-	y = input("Press any key to view first summary.")
+	for num, j in enumerate(shuf):
 
-	print()
+		print("Automatic summary " + str(num) + ":")
 
-	for line in msdata:
-		print(line, end="")
+		for line in data[j]:
+			print(line, end="")
+		print('\n')
 
-	print()
+		r = int(input("Rate the RELEVANCE for this summary on as scale from 1 and 5:"))
+		scores[j][0].append(r)
 
-	y = input("Press any key to view second summary.")
+		print('\n')
 
-	print()
+		c = int(input("Rate the COHERENCE for this summary on as scale from 1 and 5:"))
+		scores[j][1].append(c)
 
-	for line in pndata:
-		print(line, end="")
+		print('\n')
 
-	print()
+	rrow = [scores[j][0][i] for j in range(4)]
+	crow = [scores[j][1][i] for j in range(4)]
 
-	r = input("Enter which was more relevant (0 or 1):")
-	c = input("Enter which was more coherent (0 or 1):")
+	with open('./rout.csv', 'a', newline='') as f:
+		w = writer(f)
+		w.writerow(rrow)
 
-	if (r == "0"):
-		msscore[0] += 1
-	else:
-		pnscore[0] += 1
-
-	if (c == "0"):
-		msscore[1] += 1
-	else:
-		pnscore[1] += 1
+	with open('./cout.csv', 'a', newline='') as f:
+		w = writer(f)
+		w.writerow(crow)
