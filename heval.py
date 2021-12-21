142304a6a3fb9509a5ccc12663f0b8bb51842134
fbarn <filippo.baronello@mail.mcgill.ca>
2021-12-20 21:00:05 -0500

    finished heval

--- heval.py
+++ heval.py
@@ -6,7 +6,6 @@ ms = sorted(listdir('./dec/MatchSum/'), key=lambda f : int(f.split(".")[0]))
 pn = sorted(listdir('./dec/PNBERT/'), key=lambda f : int(f.split(".")[0]))
 pg = sorted(listdir('./dec/PGen/'), key=lambda f : int(f.split(".")[0]))
 ls = sorted(listdir('./dec/LoopSum/'), key=lambda f : int(f.split(".")[0]))
-rf = sorted(listdir('./ref/'), key=lambda f : int(f.split(".")[0]))
 inputs = sorted(listdir('./in/'), key=lambda f : int(f.split(".")[0]))
 
 msscore = [[], []]
@@ -18,15 +17,12 @@ scores = [msscore, pnscore, pgscore, lsscore]
 
 for i in range(50):
 
-	shufe = [j for j in range(2)]
-	shufa = [j for j in range(2,4)]
-	shuffle(shufe)
-	shuffle(shufa)
+	shuf = [j for j in range(4)]
+	shuffle(shuf)
 
 	data = [[] for i in range(4)]
 
 	indata = []
-	rfdata = []
 
 
 	with open('./dec/MatchSum/' + ms[i], "r") as _file:
@@ -37,8 +33,7 @@ for i in range(50):
 		data[2] = _file.readlines()
 	with open('./dec/LoopSum/' + ls[i], "r") as _file:
 		data[3] = _file.readlines()
-	with open('./ref/' + rf[i], "r") as _file:
-		rfdata = _file.readlines()
+
 	with open('./in/' + inputs[i], "r") as _file:
 		indata = _file.readlines()
 
@@ -51,42 +46,23 @@ for i in range(50):
 
 	print('\n')
 
-	print("Reference summary:")
+	for num, j in enumerate(shuf):
 
-	for line in rfdata:
-		print(line, end="")
+		print("Automatic summary " + str(num) + ":")
 
-	print('\n')
+		for line in data[j]:
+			print(line, end="")
+		print('\n')
 
-	for name,shuf in [("Extractive", shufe), ("Abstractive", shufa)]:
-		for num, j in enumerate(shuf):
+		r = int(input("Rate the RELEVANCE for this summary on as scale from 1 and 5:"))
+		scores[j][0].append(r)
 
-			print(name + " summary " + str(num) + ":")
+		print('\n')
 
-			for line in data[j]:
-				print(line, end="")
-			print('\n')
+		c = int(input("Rate the COHERENCE for this summary on as scale from 1 and 5:"))
+		scores[j][1].append(c)
 
-		for num, j in enumerate(shuf):
-			r=-1
-			while (type(r)!=float or r >5 or r <0):
-				try:
-					r = float(input("Rate the RELEVANCE for " + name.lower() + " summary " + str(num) +" on a scale from 1 and 5:"))
-				except ValueError:
-					continue
-			scores[j][0].append(r)
-
-			print('\n')
-
-			c=-1
-			while (type(c)!=float or c >5 or c <0):
-				try:
-					c = float(input("Rate the COHERENCE for " + name.lower() + " summary " + str(num) +" on a scale from 1 and 5:"))
-				except ValueError:
-					continue
-			scores[j][1].append(c)
-
-			print('\n')
+		print('\n')
 
 	rrow = [scores[j][0][i] for j in range(4)]
 	crow = [scores[j][1][i] for j in range(4)]
